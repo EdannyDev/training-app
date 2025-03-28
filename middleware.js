47958ec5917ccdb8082603d182c2f6f66be6888a
DarkDanny25 <edanuc15@gmail.com>
2025-03-28 17:42:53 -0600

    ajustes

--- middleware.js
+++ middleware.js
@@ -3,6 +3,7 @@ import { NextResponse } from 'next/server';
 export function middleware(req) {
   const allowedIps = ['10.147.20.13', '10.147.20.174'];
   const clientIp = req.ip || req.headers.get('x-forwarded-for');
+  console.log('Client IP:', clientIp);
 
   if (!allowedIps.includes(clientIp)) {
     return new NextResponse('Acceso denegado', { status: 403 });
