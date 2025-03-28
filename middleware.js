b1537d372f4515f931e6cc02c578825ab8e90cf7
DarkDanny25 <edanuc15@gmail.com>
2025-03-28 17:47:40 -0600

    ajustes al frontend

--- middleware.js
+++ middleware.js
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(req) {
+  console.log('Headers:', req.headers);
+  
   const allowedIps = ['10.147.20.13', '10.147.20.174'];
   const clientIp = req.ip || req.headers.get('x-forwarded-for');
   console.log('Client IP:', clientIp);
