4987bdb46e53cebcd1501d3ff3a920c02ccbc6ed
DarkDanny25 <edanuc15@gmail.com>
2025-03-28 17:55:15 -0600

    ajustes nuevos

--- middleware.js
+++ middleware.js
@@ -3,10 +3,13 @@ import { NextResponse } from 'next/server';
 export function middleware(req) {
   console.log('Headers:', req.headers);
   
-  const allowedIps = ['10.147.20.13', '10.147.20.174'];
-  const clientIp = req.ip || req.headers.get('x-forwarded-for');
+  // Obtener la IP real desde el encabezado 'x-forwarded-for'
+  const forwardedFor = req.headers.get('x-forwarded-for');
+  const clientIp = forwardedFor ? forwardedFor.split(',')[0] : req.ip;
   console.log('Client IP:', clientIp);
 
+  const allowedIps = ['10.147.20.13', '10.147.20.174'];
+  
   if (!allowedIps.includes(clientIp)) {
     return new NextResponse('Acceso denegado', { status: 403 });
   }
