aa6f2745161f939a750d1794b76306bcfb5a9cb8
DarkDanny25 <edanuc15@gmail.com>
2025-03-28 18:02:25 -0600

    cambios

--- middleware.js
+++ middleware.js
@@ -2,14 +2,11 @@ import { NextResponse } from 'next/server';
 
 export function middleware(req) {
   console.log('Headers:', req.headers);
-  
-  // Obtener la IP real desde el encabezado 'x-forwarded-for'
-  const forwardedFor = req.headers.get('x-forwarded-for');
-  const clientIp = forwardedFor ? forwardedFor.split(',')[0] : req.ip;
+  const clientIp = req.socket.remoteAddress; // Usamos la IP directa
   console.log('Client IP:', clientIp);
 
-  const allowedIps = ['10.147.20.13', '10.147.20.174'];
-  
+  const allowedIps = ['10.147.20.13', '10.147.20.174']; // Asegúrate de tener las IPs correctas de ZeroTier
+
   if (!allowedIps.includes(clientIp)) {
     return new NextResponse('Acceso denegado', { status: 403 });
   }
