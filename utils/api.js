e84adca8ea243f1831a3c9dfd4685e56a4287ddd
DarkDanny25 <edanuc15@gmail.com>
2025-05-06 19:57:39 -0600

    activando sitio web frontend

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000/api',
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://backend-training-jo66.onrender.com/api',
 });
 
 API.interceptors.request.use(
